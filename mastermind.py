48d0dadf5c99b10767d3e60264067c6527342b00
Quyen Mac <quyenquyenmac@gmail.com>
2015-12-07 23:34:12 -0500

    added random generator

--- mastermind.py
+++ mastermind.py
@@ -1,2 +1,36 @@
 import socket
+import random
+import string
 
+
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = socket.gethostname()
+	port = 9000
+	s.bind((host, port))
+
+	s.listen(10)
+	count = 0
+	win = False
+	while True:
+		conn, addr = s.accept()
+		print 'Connecting with ', addr
+		if (count < 10) or (win = True):
+			break
+		else:
+			guess = conn.recv(1024)
+			if not guess:
+				pass
+			else:
+                # generate list of random symbols
+				symbol = ['!', '@', '#', '$', '%', '^']
+                combination = (random.sample(symbol,4))
+
+                
+
+
+		conn.close()
+
+
+if __name__ == '__main__':
+	main()
